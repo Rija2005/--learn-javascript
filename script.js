bd5cb0a5897d46d1368f12a9aad3f39549bc17db
Rija2005 <164767074+Rija2005@users.noreply.github.com>
2024-05-06 16:35:33 +0500

    mypractice

---
+++ script.js
@@ -0,0 +1,125 @@
+// console.log("hello world");
+
+// document.write("learn javaascript");
+
+// alert("message on display screen");
+
+// var firstname = "rija";
+// console.log(firstname);                                                                                                                                                                     
+
+
+
+//  var dateOfBirth; //declare
+//  dateOfBirth = "21-june-2005";//initialize
+//  console.log(dateOfBirth);   
+
+// var name ="rija fatima";
+
+// var age = 18;
+
+// var email = "syedarija2005@gmail.com";
+
+// console.log(" My name is "  + name +  " My age is "  + age +  " My email is "  +   email  );
+
+// var message = " hello in javascript world ";
+// alert(message);
+
+
+// var hello;
+// console.log(hello);//undefined
+
+// var lastnane = "Rija";
+// var lastnane = "Fatima";
+// console.log(lastnane);
+
+
+// let firstname = "umer";
+
+
+
+// let x = 12;
+// let y = 3;
+// // let result = x * y;
+// console.log("the multiplication of two numbers is:" +  (x * y) );
+// console.log("the division of two numbers is:" +  (x / y) );
+// console.log("the addition of two numbers is:" +  (x + y) );
+// console.log("the subtraction of two numbers is:" +  (x - y) );
+// console.log("the modulus of two numbers is:" +  (x % y) );
+
+// //legal variable
+// let Firstname;
+// let lastnane;
+// let _dateOfBirth;
+// let $email;
+// let age21;
+
+
+// //illegal variable
+// let 12;
+// let 12return;
+// let !hello;
+// let @34question;
+// let "wasfa";
+
+
+let rose = "flower";
+console.log(Rose);
+
+
+
+//QUESTION NO1
+let onevar = "one"; secondvar = "second"; thirdvar = "third";
+
+// //QUESTION NO2 
+
+// //legal variables 
+// let firstname = "rija";
+// let lastnane = "fatima";
+// let _dateOfBirth;
+// let $email;
+// let age21;
+
+// //illegal variables 
+// let 12;
+// let 12return;
+
+
+
+//  //QUESTION NO 3
+
+// let x = 12;
+// let y = 3;
+// // let result = x * y;
+
+// console.log("the addition of two numbers is:" +  (x + y) );
+
+
+//QUESTION NO 4 
+
+ let x = 12;
+let y = 3;
+// let result = x * y;
+console.log("the multiplication of two numbers is:" +  (x * y) );
+console.log("the division of two numbers is:" +  (x / y) );
+console.log("the addition of two numbers is:" +  (x + y) );
+console.log("the subtraction of two numbers is:" +  (x - y) );
+console.log("the modulus of two numbers is:" +  (x % y) );
+
+
+//QUESTION NO 5 
+
+let costOfTicket = 600;
+let buyingticket = 5;
+let result =costOfTicket * buyingticket;
+ console.log("total cost:" +  (costOfTicket * buyingticket) );
+
+
+
+
+
+
+
+
+
+
+
